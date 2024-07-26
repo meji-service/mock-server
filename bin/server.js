3888c84e5074fe70f402c92269bcabfad0962a43
lwz <liangweizhu@siteshun.com>
2024-07-26 15:01:15 +0800

    fix: 修复bin 文件少了#! /usr/bin/env node 环境声明，导致npm 下载的无法启动改服务

--- bin/server.js
+++ bin/server.js
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 const { program } = require('commander');
 const pkg = require('../package.json');
 program.version(pkg.version, '-v, --version').usage('<command> [options]');
